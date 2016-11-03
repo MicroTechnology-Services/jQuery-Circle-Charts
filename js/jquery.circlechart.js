4ca0e43d4bd6140516a557028b4082b849c9bf8d
Neil Gaylor <neil@mitsi.com>
2016-11-03 12:43:12 -0500

    Whitespace

--- js/jquery.circlechart.js
+++ js/jquery.circlechart.js
@@ -1,10 +1,10 @@
-(function($){
+(function($) {
    $.fn.extend({
-       //pass the options variable to the function
-      percentcircle: function(options) {
-      //Set the default values, use comma to separate the settings, example:
+      // pass the options variable to the function
+      circleChart: function(options) {
+      // Set the default values, use comma to separate the settings, example:
          var defaults = {
-                 animate : true,
+               animate : true,
                diameter : 100,
                guage: 2,
                coverBg: '#fff',
@@ -14,28 +14,28 @@
                percentWeight: 'normal'
             },
             styles = {
-                cirContainer : {
-                   'width':defaults.diameter,
-                  'height':defaults.diameter
+               cirContainer : {
+                  'width':defaults.diameter,
+                 'height':defaults.diameter
                },
                cir : {
-                   'position': 'relative',
-                   'text-align': 'center',
-                   'width': defaults.diameter,
-                   'height': defaults.diameter,
-                   'border-radius': '100%',
-                   'background-color': defaults.bgColor,
-                   'background-image' : 'linear-gradient(91deg, transparent 50%, '+defaults.bgColor+' 50%), linear-gradient(90deg, '+defaults.bgColor+' 50%, transparent 50%)'
+                  'position': 'relative',
+                  'text-align': 'center',
+                  'width': defaults.diameter,
+                  'height': defaults.diameter,
+                  'border-radius': '100%',
+                  'background-color': defaults.bgColor,
+                  'background-image' : 'linear-gradient(91deg, transparent 50%, '+defaults.bgColor+' 50%), linear-gradient(90deg, '+defaults.bgColor+' 50%, transparent 50%)'
                },
                cirCover: {
                   'position': 'relative',
-                   'top': defaults.guage,
-                   'left': defaults.guage,
-                   'text-align': 'center',
-                   'width': defaults.diameter - (defaults.guage * 2),
-                   'height': defaults.diameter - (defaults.guage * 2),
-                   'border-radius': '100%',
-                   'background-color': defaults.coverBg
+                  'top': defaults.guage,
+                  'left': defaults.guage,
+                  'text-align': 'center',
+                  'width': defaults.diameter - (defaults.guage * 2),
+                  'height': defaults.diameter - (defaults.guage * 2),
+                  'border-radius': '100%',
+                  'background-color': defaults.coverBg
                },
                text: {
                   'display': 'table-cell',
@@ -58,14 +58,14 @@
             };
 
          var that = this,
-               template = '<div><div class="ab"><div class="cir"><div class="text"><div class="inner"><div class="title">{{title}}</div><div class="perc">{{percentage}}</div></div></div></div></div></div>',
-               options =  $.extend(defaults, options)
+            template = '<div><div class="ab"><div class="cir"><div class="text"><div class="inner"><div class="title">{{title}}</div><div class="perc">{{percentage}}</div></div></div></div></div></div>',
+            options =  $.extend(defaults, options)
 
-         function init(){
-            that.each(function(){
+         function init() {
+            that.each(function() {
                var $this = $(this),
-                   //we need to check for a percent otherwise set to 0;
-                  perc = Math.round($this.data('percent')), //get the percentage from the element
+                  // we need to check for a percent otherwise set to 0;
+                  perc = Math.round($this.data('percent')), // get the percentage from the element
                   title = $this.data('title') || '',
                   deg = perc * 3.6,
                   stop = options.animate ? 0 : deg,
@@ -74,7 +74,7 @@
                      altLabel !== 'undefined' ? altLabel : perc+'%')
                      .replace('{{title}}',title)
                   );
-                  //set all of the css properties forthe chart
+                  // set all of the css properties forthe chart
                   $chart.css(styles.cirContainer)
                      .find('.ab').css(styles.cir)
                      .find('.cir').css(styles.cirCover)
@@ -83,30 +83,29 @@
                      .find('.title').css(styles.title)
                      .siblings('.perc').css(styles.percent);
 
-               $this.append($chart); //add the chart back to the target element
-               setTimeout(function(){
-                  animateChart(deg,parseInt(stop),$chart.find('.ab')); //both values set to the same value to keep the function from looping and animating
-               },250)
-               });
+               $this.append($chart); // add the chart back to the target element
+               setTimeout( function() {
+                  animateChart(deg,parseInt(stop),$chart.find('.ab')); // both values set to the same value to keep the function from looping and animating
+               }, 250)
+            });
          }
 
-         var animateChart = function (stop,curr,$elm){
+         var animateChart = function (stop,curr,$elm) {
             var deg = curr;
-            if(curr <= stop){
-               if (deg>=180){
+            if(curr <= stop) {
+               if (deg >= 180) {
                   $elm.css('background-image','linear-gradient(' + (90+deg) + 'deg, transparent 50%, '+options.fillColor+' 50%),linear-gradient(90deg, '+options.fillColor+' 50%, transparent 50%)');
-                }else{
-                   $elm.css('background-image','linear-gradient(' + (deg-90) + 'deg, transparent 50%, '+options.bgColor+' 50%),linear-gradient(90deg, '+options.fillColor+' 50%, transparent 50%)');
-                }
+               } else {
+                  $elm.css('background-image','linear-gradient(' + (deg-90) + 'deg, transparent 50%, '+options.bgColor+' 50%),linear-gradient(90deg, '+options.fillColor+' 50%, transparent 50%)');
+               }
                curr ++;
-               setTimeout(function(){
+               setTimeout( function() {
                   animateChart(stop,curr,$elm);
-               },1);
+               }, 1);
             }
          };
-
-         init(); //kick off the goodness
-          }
+         init(); // kick off the goodness
+      }
    });
 
 })(jQuery);
